09acd892818ece44205f5ef33c8bc93e47c2935c
valentinesamuel <valentinesamuel2580@gmail.com>
2024-10-31 19:28:24 +0100

    feat: getting user and userFollowers

--- src/managers/user.manager.ts
+++ src/managers/user.manager.ts
@@ -10,18 +10,147 @@ export class UserManager {
   getUser(username: string) {
     const res = this.client._request({
       query: `
-            query GetUserById($input: String!) {
-                user(username: $input) {
-                    id
-                    name
-                    username
-                }
+        query GetUserByUsername($input: String!) {
+          user(username: $input) {
+            id
+            name
+            username
+            bio {
+              markdown
+              text
             }
-            `,
+            profilePicture
+            socialMediaLinks {
+              website
+              github
+              twitter
+              instagram
+              facebook
+              stackoverflow
+              linkedin
+              youtube
+            }
+            badges {
+              id
+              name
+              description
+              image
+              dateAssigned
+              infoURL
+              suppressed
+            }
+            followersCount
+            followingsCount
+            tagline
+            dateJoined
+            location
+            availableFor
+            deactivated
+            following
+            followsBack
+            isPro
+          }
+        }
+          `,
       variables: {
         input: username,
       },
     });
     return res;
   }
+
+  getMe() {
+    const res = this.client._request({
+      query: `
+          me {
+            id
+            name
+            username
+            bio {
+              markdown
+              text
+            }
+            profilePicture
+            socialMediaLinks {
+              website
+              github
+              twitter
+              instagram
+              facebook
+              stackoverflow
+              linkedin
+              youtube
+            }
+            badges {
+              id
+              name
+              description
+              image
+              dateAssigned
+              infoURL
+              suppressed
+            }
+            followersCount
+            followingsCount
+            tagline
+            dateJoined
+            location
+            availableFor
+            deactivated
+            role
+          }
+        `,
+    });
+    return res;
+  }
+
+  getUserFollowers(username: string, page: number, pageSize: number) {
+    const res = this.client._request({
+      query: `
+query GetUserFollowers($username: String!, $page: Int!, $pageSize: Int!) {
+    user(username: $username) {
+    followers(pageSize: $pageSize, page: $page) {
+      nodes {
+        id
+        username
+        bio {
+          markdown
+        }
+        profilePicture
+        socialMediaLinks{
+          website
+          github
+          twitter
+          instagram
+          facebook
+          stackoverflow
+          linkedin
+          youtube
+        }
+        dateJoined
+        isPro
+        following
+        deactivated
+        tagline
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        previousPage
+        nextPage
+      }
+      totalDocuments
+    }
+  }
+}
+      `,
+      variables: {
+        username,
+        page,
+        pageSize,
+      },
+    });
+    return res;
+  }
+
 }

8fbe70dd3e306f7e8fb03ba57b5b6d7ce5ba14d0
valentinesamuel <valentinesamuel2580@gmail.com>
2024-10-31 16:10:37 +0100

    feat: uncommented test request files

--- testRequest.js
+++
@@ -1,8 +0,0 @@
-import { HashnodeSDKClient } from './dist/index.js';
-
-const client = new HashnodeSDKClient({
-  apiKey: 'YOUR_API_KEY',
-});
-
-const user = await client.userManager.getUser('engineeringval');
-console.log(user);

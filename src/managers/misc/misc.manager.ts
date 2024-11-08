4716cbde322f19deddbc8e5579bb35cee8806799
valentinesamuel <valentinesamuel2580@gmail.com>
2024-11-08 15:26:51 +0100

    feat: added miscelleanous

---
+++ src/managers/misc/misc.manager.ts
@@ -0,0 +1,47 @@
+import { BaseManager } from '../base.manager';
+import type { HashnodeSDKClient } from '../../client';
+import { GET_TAG_BY_SLUG_QUERY, GET_TOP_COMMENTERS_QUERY } from './misc.queries';
+import type { CommenterUserConnection, Tag } from '../../generated/gqlQueryTypes';
+
+
+/**
+ * Manages random operations.
+ *
+ * @extends BaseManager
+ */
+export class MiscellaneousManager extends BaseManager {
+  /**
+   * Creates an instance of MiscellaneousManager.
+   *
+   * @param client - The Hashnode SDK client.
+   */
+  constructor(client: HashnodeSDKClient) {
+    super(client, 'MiscellaneousManager');
+  }
+
+  /**
+   * Returns the top users who have most actively participated in discussions by commenting in the last 7 days
+   * @param after - The cursor to start the query from.
+   * @param first - The number of users to return.
+   * @returns The top users who have most actively participated in discussions by commenting in the last 7 days.
+   */
+  async getTopCommenters(first: number, after?: string) {
+    const res = await this.makeRequest<{ topCommenters: CommenterUserConnection }>(
+      'getTopCommenters',
+      GET_TOP_COMMENTERS_QUERY,
+      { first, after },
+    );
+    return res.topCommenters;
+  }
+
+  /**
+   * Returns tag details by its slug
+   * 
+   * @param slug - The slug of the tag.
+   * @returns The tag details.
+   */
+  async getTagBySlug(slug: string) {
+    const res = await this.makeRequest<{ tag: Tag }>('getTagBySlug', GET_TAG_BY_SLUG_QUERY, { slug });
+    return res.tag;
+  }
+}
---
+++ src/managers/misc/misc.queries.ts
@@ -0,0 +1,83 @@
+import { gql } from 'graphql-request';
+
+export const GET_TOP_COMMENTERS_QUERY = gql`
+query getTopCommenters($first: Int!, $after: String) {
+  topCommenters(first: $first, after: $after) {
+    edges {
+      node {
+        id
+        username
+        name
+        bio {
+          markdown
+        }
+        profilePicture
+        socialMediaLinks {
+          website
+          github
+          twitter
+          instagram
+          facebook
+          stackoverflow
+          linkedin
+          youtube
+        }
+        badges {
+          id
+          name
+          description
+          image
+          image
+          dateAssigned
+          infoURL
+          suppressed
+        }
+        followersCount
+        followingsCount
+        tagline
+        dateJoined
+        location
+        availableFor
+        tagsFollowing {
+          id
+          name
+          slug
+          logo
+          tagline
+          info {
+            markdown
+          }
+          followersCount
+          postsCount
+        }
+        deactivated
+        following
+        followsBack
+        isPro
+      }
+      cursor
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+`;
+
+export const GET_TAG_BY_SLUG_QUERY = gql`
+query getTagBySlug($slug: String!) {
+  tag(slug: $slug) {
+    id
+    name
+    slug
+    logo
+    tagline
+    info {
+      markdown
+    }
+    followersCount
+    postsCount
+  }
+}
+`
